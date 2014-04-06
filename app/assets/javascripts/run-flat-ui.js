2d94bbe56007bd243f97e024e47a284b92b2222b
Tony Han <h.bing612@gmail.com>
2014-04-07 01:23:00 +0800

    add search form for index page

---
+++ app/assets/javascripts/run-flat-ui.js
@@ -0,0 +1,13 @@
+(function($) {
+
+  $(function() {
+
+    // Focus state for append/prepend inputs
+    $('.input-group').on('focus', '.form-control', function () {
+      $(this).closest('.input-group, .form-group').addClass('focus');
+    }).on('blur', '.form-control', function () {
+      $(this).closest('.input-group, .form-group').removeClass('focus');
+    });
+  });
+
+})(jQuery);
