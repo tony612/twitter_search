d5186560cb516e3fab0204ccc19452ddbde610c2
Tony Han <h.bing612@gmail.com>
2014-04-22 01:17:26 +0800

    add page for normal search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,17 @@ class SearchController < ApplicationController
   end
 
   def search
-    redirect_to root_path
+    query = params[:q]
+    if query.present?
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+        config.bearer_token    = ENV['TWITTER_BEARER_TOKEN']
+      end
+      @tweets = client.search(query, :result_type => "recent").take(20)
+    else
+      flash[:warning] = 'Please input the keywords'
+      @tweets = []
+    end
   end
 end
