d8a157b28f944728411ac1782be793511aaddada
Tony Han <h.bing612@gmail.com>
2014-05-09 14:48:09 +0800

    change result_type and lang of search endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,14 +4,14 @@ class SearchController < ApplicationController
 
   def search
     query = params[:q]
-    result_type = params[:type] || 'recent'
+    result_type = params[:type] || 'mixed'
     if query.present?
       client = Twitter::REST::Client.new do |config|
         config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
         config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
         config.bearer_token    = ENV['TWITTER_BEARER_TOKEN']
       end
-      @tweets = client.search(query, :result_type => result_type).take(20)
+      @tweets = client.search(query, :result_type => result_type, lang: 'en').take(100)
     else
       flash[:warning] = 'Please input the keywords'
       @tweets = []
