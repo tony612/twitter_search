50d7d6802aec222ae6cd62037b245d88e31d02b4
Tony Han <h.bing612@gmail.com>
2014-05-13 12:43:48 +0800

    add new to words_bag

--- lib/twitter_helper.rb
+++ lib/twitter_helper.rb
@@ -12,6 +12,7 @@ class TwitterHelper
       she should since so some than that the their them then there these they this
       tis to too twas us wants was we were what when where which while who whom why
       will with would yet you your want rt twitter use good cool cc have out via
+      new
     }
 
     def handle_rate_limit
